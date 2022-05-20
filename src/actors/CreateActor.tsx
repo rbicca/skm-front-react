ebfd8034a98e8d70abbf6346d97db0d09a5c9c6e
Ronaldo Bicca <ronaldo.bicca@gmail.com>
2022-05-19 21:40:39 -0300

    Cap 134

--- src/actors/CreateActor.tsx
+++ src/actors/CreateActor.tsx
@@ -1,14 +1,43 @@
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { urlActors } from "../endpoints";
+import DisplayErrors from "../utils/DisplayErrors";
+import { convertActorToFormData } from "../utils/formDataUtil";
 import ActorForm from "./ActorForm";
+import { actorCreationDTO } from "./actors.model";
 
 export default function CreateActor(){
+
+    const [errors, setErrors] = useState<string[]>([]);
+    const history = useHistory();
+
+    async function create(actor: actorCreationDTO){
+        try{
+            const formData = convertActorToFormData(actor);
+
+            await axios({
+                method: 'post',
+                url: urlActors,
+                data: formData,
+                headers: {'Content-Type':'multipart/form-data'}
+            });
+            history.push('/actors');
+            
+        } catch(error){
+            if(error && error.response){
+                setErrors(error.response.data);
+            }
+        }
+    }
 
     return (
         <>
             <h3>Novo ator</h3>
-
+            <DisplayErrors errors={errors}/>
             <ActorForm 
                 model={{name: '', dateOfBirth: undefined}}
-                onSubmit={values => console.log(values)}
+                onSubmit={ async values => await create(values)}
             />
         </>
     );
